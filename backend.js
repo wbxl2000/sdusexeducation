48e65fb37188284e6f2630284721a981d2d20782
qer <wbxl2000@outlook.com>
2021-06-13 16:28:35 +0800

    add .vercel

--- backend.js
+++ backend.js
@@ -5,8 +5,21 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+app.all("*",function(req,res,next){
+  res.setHeader('Access-Control-Allow-Credentials', true)
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  )
+  if (req.method.toLowerCase() == 'options')
+    res.send(200).end();  //让options尝试请求快速结束
+  else
+    next();
+});
+
 const request = require('request');
- 
 let options = {
   url: 'http://seat.lib.sdu.edu.cn/api.php/users/201805130163',
   json: true,
